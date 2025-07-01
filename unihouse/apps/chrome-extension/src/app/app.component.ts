a9bde91aedc00cbf91a8b2438cbec6ccc43b3f7d
Mitchell Cassaday <casmit2884@gmail.com>
2025-07-01 12:51:21 -0400

    updated app component header

--- unihouse/apps/chrome-extension/src/app/app.component.ts
+++ unihouse/apps/chrome-extension/src/app/app.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { TuiAppearance, TuiButton, TuiError, TuiIcon, TuiNotification, TuiRoot, TuiTextfield, TuiTitle } from "@taiga-ui/core";
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { AsyncPipe, CommonModule } from "@angular/common";
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [
+    NxWelcomeComponent, 
+    CommonModule,
+    RouterModule, 
+    TuiRoot,
+    AsyncPipe,
+    ReactiveFormsModule,
+    TuiAppearance,
+    TuiButton,
+    TuiError,
+    TuiIcon,
+    TuiNotification,
+    TuiTextfield,
+    TuiTitle,
+    
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
@@ -11,6 +29,18 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 export class AppComponent {
   title = 'chrome-extension';
   entryid: number = 427786;
+  private fb: FormBuilder = inject(FormBuilder);
+  form: FormGroup;
+
+  constructor() {
+    this.form = this.fb.group({
+      "basic" : [],
+      "email" : [],
+      "name" : [],
+      "subscribe" : [],
+    }
+    )
+  }
   async test() {
     
     const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
