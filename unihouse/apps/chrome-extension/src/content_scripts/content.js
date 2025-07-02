be5a132c7b69a255bdc9d64bad9a2949a002cbb7
Mitchell Cassaday <casmit2884@gmail.com>
2025-07-02 09:31:23 -0400

    added username fetch script

--- unihouse/apps/chrome-extension/src/content_scripts/content.js
+++ unihouse/apps/chrome-extension/src/content_scripts/content.js
@@ -1,6 +1,14 @@
 
 // contentScript.js
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.message === 'sap_url_changed') {
-  }
+  if (request.action === 'sap_url_changed') {
+  }else if (request.action === 'get_user') {    
+    let username_span = document.querySelector("#header-user-menu > article > ul > li:nth-child(1) > span");
+    if (username_span) {
+      sendResponse( username_span.innerHTML)
+    } else {
+      sendResponse({ data: null })
+    }
+  } else { sendResponse('no action') }
 });
+console.log('listening')
