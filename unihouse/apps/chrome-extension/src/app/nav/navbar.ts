50dffdefd4c2c7539be48c5d55d568bf0a3d719d
Mitchell Cassaday <casmit2884@gmail.com>
2025-07-02 09:30:05 -0400

    created the nav bar component

---
+++ unihouse/apps/chrome-extension/src/app/nav/navbar.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [CommonModule],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.scss',
+})
+export class Navbar {}
