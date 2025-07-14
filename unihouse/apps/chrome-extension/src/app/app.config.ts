53bb4aa50383104102d43384e991567eea59574f
Mitchell Cassaday <casmit2884@gmail.com>
2025-07-14 13:03:00 -0400

    implemented the starrez http interceptor

--- unihouse/apps/chrome-extension/src/app/app.config.ts
+++ unihouse/apps/chrome-extension/src/app/app.config.ts
@@ -3,12 +3,18 @@ import { provideAnimations } from "@angular/platform-browser/animations";
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { AuthHeaderInterceptor } from "./core/starrez/starrez-http.interceptor";
 
 export const appConfig: ApplicationConfig = {
   providers: [
         provideAnimations(),
         provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
-        provideEventPlugins()
+        provideEventPlugins(),{
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthHeaderInterceptor,
+      multi: true
+    }
     ],
 };
