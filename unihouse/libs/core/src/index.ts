221e886e8d9d3258d1f52b3e1341a8fb7c1a2ae3
Mitchell Cassaday <casmit2884@gmail.com>
2025-07-14 15:23:08 -0600

    updated list items

--- unihouse/libs/core/src/index.ts
+++ unihouse/libs/core/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/core/core';
 export * from './lib/pagination';
 export * from './lib/storage/local-storage.service';
 export * from './lib/avatars/initials-avatar';
+export * from './lib/lists/list-item';
--- unihouse/libs/core/src/lib/lists/list-item.ts
+++ unihouse/libs/core/src/lib/lists/list-item.ts
@@ -1,15 +1,50 @@
+import { CommonModule } from "@angular/common";
+import { Component, input, output } from "@angular/core";
+import { FormControl, ReactiveFormsModule } from "@angular/forms";
+import { TuiCheckbox } from "@taiga-ui/kit";
 
-
-class GenericListItem<T> {
-    private _is_selected: boolean = false;
+@Component({
+selector: 'uh-list-item',
+  imports: [CommonModule,ReactiveFormsModule,
+    TuiCheckbox
+  ],
+  template: `	
+    <div id="list-item-container">
+        <div id="checkbox-container">
+            <input
+                tuiCheckbox
+                type="checkbox"
+                [formControl]="formControl"
+                (click)="toggle()"
+            />
+        </div>
+        <div id="content-outlet">
+            <ng-content/>
+        </div>
+    </div>
+  `,
+  styles: `
+    #list-item-container { display: flex; }
+    #checkbox-container { margin: auto 0; }
+    #content-outlet { flex-grow: 1; padding: 5px 10px; }
+    `
+})
+export class GenericListItem<T> {
+    protected _is_selected: boolean = false;
     private _index: number = -1;
-    item_value!: T;
+    toggled = output<boolean>();
+    item_value = input<T>();
+    protected formControl = new FormControl(false);
+
     constructor() {
 
     }
 
     public toggle() {
+        console.log('toggle')
         this._is_selected = ! this._is_selected;
+        this.formControl.setValue(this._is_selected);
+        this.toggled.emit(this._is_selected);
     }
 
     /** Update index to the desired location in a list */
