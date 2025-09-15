7f3bde3e59231d269aac53c6a664d53044abb077
Mitchell Cassaday <casmit2884@gmail.com>
2025-09-15 15:43:30 -0400

    updated environment table

--- unihouse/libs/core/src/lib/environments/environment-list/environment-list.component.ts
+++ unihouse/libs/core/src/lib/environments/environment-list/environment-list.component.ts
@@ -1,12 +1,17 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ExtEnvironmentService, ExternalEnvironment } from '../ext-environment.service';
 import { DynamicTableComponent } from '../../tables/dynamic-table/dynamic-table.component';
+import { TableColumnOption } from '../../tables';
+import { NamedTemplateDirective } from '../../directives';
+import { TuiBadge } from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-environment-list',
   standalone: true,
   imports: [
-    DynamicTableComponent
+    DynamicTableComponent,
+    NamedTemplateDirective,
+    TuiBadge
   ],
   templateUrl: './environment-list.component.html',
   styleUrls: ['./environment-list.component.scss']
@@ -15,7 +20,13 @@ export class EnvironmentListComponent implements OnInit {
   private readonly environmentService = inject(ExtEnvironmentService);
   
   environments: ExternalEnvironment[] = [];
-  columns: string[] = ['id','name','url','type','prod'];
+  columns: (string|TableColumnOption)[] = [
+    'id',
+    {title: 'Environment', property: 'name'},
+    {title: 'Type', property: 'type'},
+    {title: 'URL',property: 'url'},
+    // {title: 'prod'}
+  ];
 
   constructor() {}
 
