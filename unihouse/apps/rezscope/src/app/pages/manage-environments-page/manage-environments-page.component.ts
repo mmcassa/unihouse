d30f70a8da1a1cc9d02d6108f0c0ff679cdee97c
Mitchell Cassaday <casmit2884@gmail.com>
2026-02-03 11:48:38 -0500

    abstracted add new environment form to a popup

--- unihouse/apps/rezscope/src/app/pages/manage-environments-page/manage-environments-page.component.ts
+++ unihouse/apps/rezscope/src/app/pages/manage-environments-page/manage-environments-page.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
-import { AddEnvironmentFromComponent } from '@unihouse/core';
+import { Component, inject } from '@angular/core';
+import { TuiButton } from '@taiga-ui/core';
+import { ExtEnvironmentService } from '@unihouse/core';
 import { EnvironmentListComponent } from '@unihouse/core';
 
 @Component({
   selector: 'app-manage-environments-page',
   standalone: true,
-  imports: [EnvironmentListComponent,AddEnvironmentFromComponent],
+  imports: [
+    EnvironmentListComponent,
+    TuiButton
+  ],
   templateUrl: './manage-environments-page.component.html',
   styleUrl: './manage-environments-page.component.scss'
 })
 export class ManageEnvironmentsPageComponent {
-  
+  environmentService = inject(ExtEnvironmentService);
+
+  open_new_env() {
+    this.environmentService.open_add_environment_form().subscribe({
+      next: res => {
+        
+      }
+    })
+  }
 
 }
